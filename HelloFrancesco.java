21f33b372a9199b42fdd8ebaec74c2a36c56f0de
fra957 <francesco.romano@capellinisauro.edu.it>
2025-05-12 09:15:11 +0200

    Update HelloFrancesco.java

--- HelloFrancesco.java
+++ HelloFrancesco.java
@@ -1,6 +1,17 @@
-public class HelloFrancesco {
+import java.util.vector;
 
-    public static void main(String[] args) {
-        System.out.println("Hello Francesco!");
+public class Hello{
+    public static void main(String [] args){
+        System.out.println("Hello Francesco");
+        Vector <Character> lettere= new Vector<>();
+        lettere.add(" F");
+          lettere.add(" r");
+          lettere.add(" a");
+       
+        for(Character lettera: lettere){
+            System.out.println(lettera);
+        }
+        
     }
+    
 }
