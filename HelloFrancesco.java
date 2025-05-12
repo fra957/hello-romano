f2f02dd5778176aed4c672943470a8c1065027fc
fra957 <francesco.romano@capellinisauro.edu.it>
2025-05-12 09:25:54 +0200

    Update HelloFrancesco.java

--- HelloFrancesco.java
+++ HelloFrancesco.java
@@ -1,6 +1,11 @@
-public class HelloFrancesco {
+class Hello {
+public static void main(String[] args) {
+System.out.println("Hello Francesco");
 
-    public static void main(String[] args) {
-        System.out.println("Hello Francesco!");
-    }
+char[] lettere = {'f', 'r', 'a'};
+
+for (char lettera : lettere) {
+System.out.print(lettera + " ");
+}
+}
 }
